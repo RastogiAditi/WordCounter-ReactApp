c00da08cf0c6406fd9e87c39d94ecbded7cbe6e1
Aditi Rastogi <rastogiaditi1995@gmail.com>
2023-08-26 18:19:46 +0530

    WordCounter-ReactApp

---
+++ src/App.js
@@ -0,0 +1,73 @@
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link,
+  // Outlet,
+} from "react-router-dom";
+import { useState } from 'react';
+import './App.css';
+import About from './components/About';
+import Navbar from "./components/Navbar";
+import TextForm from './components/TextForm';
+import Alert from './components/Alert';
+
+function App() {
+//dark mode enbale fumction
+  const [mode,setMode]=useState('light');
+    const [alert,setAlert]=useState(null);
+
+    const showAlert=(msg,type)=>{
+      setAlert({
+        msg:msg,
+        type:type
+
+      })
+      setTimeout(()=>{
+        setAlert(null);
+      },2000);
+    }
+
+  const toggleMode=()=>{
+    if(mode=== 'light')
+    {
+      setMode('dark');
+      document.body.style.backgroundColor='grey';
+      showAlert("Dark Mode is on....... ","success");
+    }
+    else{
+      setMode('light');
+      
+      document.body.style.backgroundColor='white';
+      showAlert("Light Mode is on....... ","success");
+    }
+  }
+  return (
+  
+    <BrowserRouter>
+    <div>
+      
+    <Navbar title="WordCounter" mode={mode} toggleMode={toggleMode}/>
+        <Alert alert={alert}/>
+        {/* <TextForm showAlert={showAlert} heading="Enter your text" mode={mode} /> */}
+    <Routes>
+    <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter your text" mode={mode} />}/>
+     <Route path="/about" element={<About />} />      
+      
+    </Routes>
+    </div>
+  </BrowserRouter>
+    
+    
+    
+
+  );
+
+   
+  
+}
+
+export default App;
+
+
+
---
+++ src/components/TextForm.js
@@ -0,0 +1,131 @@
+import React, { useState } from "react";
+
+
+function TextForm(props) {
+  //Upper Casing Method...............
+  const clickToUpperCase = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Upper Casing is done....... ", "success");
+  };
+
+  const handleOnChange = (event) => {
+    setText(event.target.value);
+  };
+
+  //  Lower Casing Method...............
+  const clickToLowerCase = () => {
+    let lowerText = text.toLowerCase();
+    setText(lowerText);
+    props.showAlert("lower Casing is done....... ", "success");
+  };
+  //Camel Casing Method...............
+  // const capitalize = (word) => {
+  //   let lower = word.toLowerCase();
+  //   return lower.charAt(0).toUpperCase() + lower.slice(1);
+  // };
+  // const clickToCamelCase = () => {
+  //   const arr = text.split(/[.]/);
+  //   // for(let i=0; i<text.split(".").length; i++){
+  //   //   arr[i].splice(text.split("."));
+  //   // }
+  //   // console.log(typeof(arr));
+  //  const camelText=[];
+  //   for (let i = 0; i < arr.length; i++) {
+  //      camelText[i] = arr[i].charAt(0).toUpperCase() +arr[i].slice(1);
+      
+  //   }
+  //    setText(camelText);
+  // };
+
+  //Clear text....................
+  const clearText = () => {
+    let clearTxt = "";
+    setText(clearTxt);
+    props.showAlert("All text is deleted. ", "success");
+  };
+  // Copytext......................
+  const copyText = () => {
+    var textCopy = document.getElementById("myBox");
+    textCopy.select();
+    navigator.clipboard.writeText(textCopy.value);
+    props.showAlert("Text is copied ", "success");
+  };
+  //Paste Text.........................
+  const pasteText = () => {
+    let textCopy = text;
+    setText(text + "\n" + textCopy);
+    props.showAlert("Text is pasted  ", "success");
+  };
+  const [text, setText] = useState("Enter your here");
+
+  return (
+    <div
+      className="container-fluid"
+      style={{ color: props.mode === "dark" ? "white" : "black" }}
+    >
+      <h1>{props.heading}</h1>
+      <div className="mb-3">
+        <textarea
+          className="form-control"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="8"
+          style={{
+            background: props.mode === "dark" ? "grey" : "white",
+            color: props.mode === "dark" ? "white" : "black",
+          }}
+        />
+      </div>
+      <div
+        className="container-fluid"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+        align="center"
+      >
+        <button className="btn btn-primary mx-2" onClick={clickToUpperCase}>
+          Covert to UpperCase
+        </button>
+        <button className="btn btn-primary mx-2" onClick={clickToLowerCase}>
+          Convert to LowerCase
+        </button>
+
+        <button className="btn btn-primary mx-2" onClick={clearText}>
+          Clear
+        </button>
+        <button className="btn btn-primary " onClick={copyText}>
+          {" "}
+          Copy
+        </button>
+        <button className="btn btn-primary mx-2" onClick={pasteText}>
+          {" "}
+          Paste
+        </button>
+
+        {/* <button className="btn btn-primary" onClick={clickToCamelCase}>
+          {" "}
+          Convert to CamelCase
+        </button> */}
+      </div>
+      <div
+        className="container"
+        style={{
+          background: props.mode === "dark" ? "light" : "dark",
+          color: props.mode === "dark" ? "white" : "black",
+        }}
+      >
+        <h2>Text Summary</h2>
+        <p>
+          {text.split(" ").length-1} Words and {text.length} Characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Time Taken To Read </p>
+        <p>{text.split(".").length-1} Sentences</p>
+        <p>{text.split("\n").length-1} Paragraphs </p>
+        <h2>Preview Text</h2>
+        <p>{text}</p>
+        
+      </div>
+    </div>
+  );
+}
+export default TextForm;
