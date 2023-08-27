7ef9d2746a454b31a942feae11481ea42356fd41
Aditi Rastogi <rastogiaditi1995@gmail.com>
2023-08-27 14:14:26 +0530

    WordCounter-ReactApp

--- src/App.js
+++ src/App.js
@@ -1,84 +1,70 @@
-// import {
-//   BrowserRouter,
-//   Routes,
-//   Route,
-//   // Link,
-//   // Outlet,
-// } from "react-router-dom";
-import { useState } from 'react';
-import './App.css';
-// import About from './components/About';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  // Link,
+  // Outlet,
+} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
-import About from './components/About';
-import Footer from './components/Footer';
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import Footer from "./components/Footer";
 
 function App() {
-//dark mode enbale fumction
-  const [mode,setMode]=useState('light');
-    const [alert,setAlert]=useState(null);
+  //dark mode enbale fumction
+  const [mode, setMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-    const showAlert=(msg,type)=>{
-      setAlert({
-        msg:msg,
-        type:type
+  const showAlert = (msg, type) => {
+    setAlert({
+      msg: msg,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 2000);
+  };
 
-      })
-      setTimeout(()=>{
-        setAlert(null);
-      },2000);
-    }
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "grey";
+      showAlert("Dark Mode is on....... ", "success");
+    } else {
+      setMode("light");
 
-  const toggleMode=()=>{
-    if(mode=== 'light')
-    {
-      setMode('dark');
-      document.body.style.backgroundColor='grey';
-      showAlert("Dark Mode is on....... ","success");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light Mode is on....... ", "success");
     }
-    else{
-      setMode('light');
-      
-      document.body.style.backgroundColor='white';
-      showAlert("Light Mode is on....... ","success");
-    }
-  }
+  };
   return (
-  
-  //   <BrowserRouter>
-  //   <div>
-      
-  //   <Navbar title="WordCounter" mode={mode} toggleMode={toggleMode}/>
-  //       <Alert alert={alert}/>
-  //       {/* <TextForm showAlert={showAlert} heading="Enter your text" mode={mode} /> */}
-  //   <Routes>
-  //   <Route path="/" element={<TextForm showAlert={showAlert} heading="Enter your text" mode={mode} />}/>
-  //    <Route path="/about" element={<About />} />      
-      
-  //   </Routes>
-  //   </div>
-  // </BrowserRouter>
+    <BrowserRouter>
     <div>
        <Navbar title="WordCounter" mode={mode} toggleMode={toggleMode}/>
         <Alert alert={alert}/>
-        <div>
-          {/* <About mode={mode}/> */}
-        <TextForm showAlert={showAlert} heading="Word Counter : Word Count| Character Count | Time Count for Reading Word" mode={mode} /> 
-  
-        </div>
-        <div><Footer/></div>
-       </div>
-    
-    
+    </div>
+    <Routes>
+      <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Word Counter : Word Count| Character Count | Time Count for Reading Word" mode={mode} />
+} />
+      <Route exact path="/about" element={<About />} />
+    </Routes>
+    <div><Footer/></div>
+  </BrowserRouter>
+);
+                              //without routing code
+    // <div>
+    //    <Navbar title="WordCounter" mode={mode} toggleMode={toggleMode}/>
+    //     <Alert alert={alert}/>
+    //     <div>
+    //       {/* <About mode={mode}/> */}
+    //     <TextForm showAlert={showAlert} heading="Word Counter : Word Count| Character Count | Time Count for Reading Word" mode={mode} />
 
-  );
-
-   
-  
+    //     </div>
+    //     <div><Footer/></div>
+    //    </div>
 }
 
 export default App;
-
-
-
